446196e5d4f174f376ef34efaee5a99fb78d784e
Edgar Costa <edgarcosta@math.dartmouth.edu>
2017-03-03 19:12:09 -0500

    doens't yet pass all tests

--- cpp/toric/dr.h
+++ cpp/toric/dr.h
@@ -12,6 +12,7 @@
 #include <cstdint>
 #include <assert.h>
 #include <stdio.h>//needed?
+#include <algorithm> //sort
 #include <map>
 #include <iostream>//needed?
 #include <fstream>//needed?
@@ -73,6 +74,13 @@ class dr{
          */
         map< Vec<int64_t>, R, vi64less> f; 
         int64_t n;
+
+        /*
+         * verbose = 0 --> silent
+         * verbose = 1 --> minimal
+         * verbose = 2 --> human readable
+         * verbose > 2 --> everything
+         */
         int64_t verbose;
         bool minimal;
         // Specifications that must match with the modulus that defines R
@@ -90,8 +98,9 @@ class dr{
         int64_t min_P(const Vec<int64_t> &v){ return ::min_P(AP, bP, v); }
         //returns the minimal k such that v \in k*int(P)
         int64_t min_intP(const Vec<int64_t> &v){ return ::min_intP(AP, bP, v); }
-        //returns true if v \in k*P  and w \notin k*P for some k, i.e., min_P(v) < min_P(w)
-        bool minPcomp(const Vec<int64_t> &v, const Vec<int64_t> &w){ return min_P(v) < min_P(w); }
+                
+
+
         
         
         // tuple_list[d] = integral points in d*P 
@@ -132,6 +141,7 @@ class dr{
          Vec< Vec< Vec<int64_t> > >cokernels_J_basis, cokernels_I_basis, basis_dr_Y, basis_dr_X;
         // reverse maps
         Vec< map< Vec<int64_t>, int64_t, vi64less> > cokernels_J_basis_dict, cokernels_I_basis_dict, basis_dr_Y_dict, basis_dr_X_dict;
+        int64_t dim_dr_Y, dim_dr_X;
 
 
         //  computes cokernels_I_basis*
@@ -167,7 +177,7 @@ class dr{
         // inclusion_matrices[u] is the matrix that represents:
         // I_u : J_0 + ... + J_n --> P_1 + P_2 + ... + P_{n+1}
         // where ai -- >  u * a_i \in P_{i + 1} and u \in P1
-        Vec< Mat<int64_t> > inclusion_matrices;
+        Vec< Mat<R> > inclusion_matrices;
         //compute inclusion_matrices
         void init_inclusion_matrices();
 
@@ -183,7 +193,7 @@ class dr{
         // projection of matrices 
         // proj_X :  PH^{n-1} (Y) ---> PH^{n-1} (X)
         // proj_notX :  PH^{n-1} (Y) ---> PH^{n-1} (X)^{perp}
-        Mat<int64_t> proj_X, proj_notX;
+        Mat<R> proj_X, proj_notX;
         //computes  proj_X and proj_notX
         void init_proj();
 
@@ -218,6 +228,7 @@ class dr{
          */
         void test_inclusion_matrices();
         void test_last_reduction();
+        void test_all();
     
 };
 
@@ -322,14 +333,20 @@ void dr<R>::init_tuples()
     
     assert_print(tuple_list[0].length(), ==, 1);
     assert_print(tuple_int_list[0].length(), ==, 0 );
-
-    cout << tuple_list << endl;
-    cout << tuple_int_list << endl;
+    
     tuple_dict.SetLength(n + 2);
     tuple_int_dict.SetLength(n + 2);
+    
+    //returns true if v \in k*P  and w \notin k*P for some k, i.e., min_P(v) < min_P(w)
+    auto minPless = [=](const Vec<int64_t> &v, const Vec<int64_t> &w){return ::min_intP(AP, bP, v) < ::min_intP(AP, bP, w);};
+ 
+
+
     for(i = 0 ; i < n + 2; i++)
     {
-        cout << i << endl;
+        sort(tuple_int_list[i].begin(), tuple_int_list[i].end(), vi64less() );
+        stable_sort(tuple_int_list[i].begin(), tuple_int_list[i].end(), minPless);
+        stable_sort(tuple_list[i].begin(), tuple_list[i].end(), minPless);
         reverse_dict(tuple_dict[i], tuple_list[i]);
         reverse_dict(tuple_int_dict[i], tuple_int_list[i]);
     }
@@ -381,62 +398,56 @@ void dr<R>::init_solve_and_cokernels()
     if(verbose > 2)
         cout<<"dr::init_solve_and_cokernels()"<<endl;
     int64_t i, j;
-    Vec<int64_t> ci;
-    ci.SetLength(n + 1);
-    for(i = 0; i < n + 1; i++)
+    Vec<ZZ> ci;
+    // ci = rank(P_i)
+    ci.SetLength(n + 2);
+    for(i = 0; i < n + 2; i++)
         ci[i] = tuple_list[i].length();
-
-    ZZX P, Q, tmp; // Q = 1 - X 
+    
+    // \sum rank(J_i) T^i = (1- T)^(n+1) \sum rank(P_i) T^i
+    ZZX P, Q, tmp; // Q = 1 - T
     SetCoeff(Q, 0, 1);
     SetCoeff(Q, 1, -1);
-    // P = sum( ci[i] * (T ** i) * (1 - T)**(self.n - i) for i in range(self.n + 1))
-    P = 0;
+    // P = (1- T)^(n+1) \sum rank(P_i) T^i
+    P = conv<ZZX>(ci);
     for(i = 0; i < n + 1; i++)
-    {
-        //tmp = ci[i] * T^i
-        LeftShift(tmp, conv<ZZX>(ci[i]), i);
-
-        //tmp = ci[i] * T^i * (1-T)^(n-i)
-        for(j = 0; j < n - i; j++)
-            tmp *= Q;
-        P+=tmp;
-    }
-    cokernels_J_dimensions.SetLength(deg(P) + 2, 0);
-    cokernels_J_dimensions[deg(P) + 1] = 0;
-    for(i = 0; i < deg(P) + 1; i++)
+        P *= Q;
+    cokernels_J_dimensions.SetLength(n + 2, 0);
+    assert_print(coeff(P, n + 1), ==, 0);
+    for(i = 0; i < n + 2; i++)
         cokernels_J_dimensions[i] = conv< long >( coeff(P, i) );
     dim_J = sum(cokernels_J_dimensions); 
 
-    if( verbose > 0 )
+    if( verbose > 1 )
         cout << "dim J_i = "<<cokernels_J_dimensions<<endl;
     
     cokernels_J_basis.SetLength(n + 2);
     cokernels_J_basis_dict.SetLength(n + 2);
+    solve_matrix.SetLength(n + 2);
+    solve_denom.SetLength(n + 2);
     for(i = n + 1; i >= 0; i--)
     {
-        if(i == n + 1 and verbose > 0)
+        if(i == n + 1 and verbose > 1)
             cout << "Asserting that the hypersurface is non degenerate" << endl;
 
         Mat<R> J;
         matrix_J(J, i);
-        if( verbose > 0)
+        if( verbose > 1)
             printf("Solving Jacobian relations at degree %ld (%ld x %ld)\n", (long)i, (long)J.NumRows(), (long)J.NumCols());
 
         Vec<int64_t> B, initB;
         if( i <= n )
         {
             initB.SetLength(cokernels_I_dimensions[i]);
-            for(j = 0; j < cokernels_I_dimensions[i]; i++)
-                initB[j] = tuple_dict[i][cokernels_I_basis[i][j]];
+            for(j = 0; j < cokernels_I_dimensions[i]; j++)
+                initB[j] = tuple_dict[i][ cokernels_I_basis[i][j] ];
 
         }
         else
         {
             initB.SetLength(0);
         }
-
         solve_system(B, solve_matrix[i], solve_denom[i], J, initB);
-        
         if(B.length() != cokernels_J_dimensions[i])
         {
             if(i == n + 1)
@@ -447,8 +458,9 @@ void dr<R>::init_solve_and_cokernels()
         if( i > 0 )
             assert( solve_matrix[i].NumRows() == (n + 1)*tuple_list[i-1].length() + B.length() );
         
+        
         // copy local data to object data
-        cokernels_J_basis[j].SetLength(B.length());
+        cokernels_J_basis[i].SetLength(B.length());
         for(j = 0; j < B.length(); j++)
             cokernels_J_basis[i][j] = tuple_list[i][B[j]];
 
@@ -466,20 +478,24 @@ void dr<R>::init_solve_and_cokernels()
     basis_dr_Y[0].SetLength(0);
     basis_dr_Y[1] = tuple_list[1];
     basis_dr_Y_dict[1] = tuple_dict[1];
+    dim_dr_Y = tuple_list[1].length();
 
     basis_dr_X[0].SetLength(0);
     basis_dr_X[1] = tuple_int_list[1];
     basis_dr_X_dict[1] = tuple_int_dict[1];
+    dim_dr_X =  tuple_int_list[1].length();
+
 
     for(i = 2; i < n + 1; i++)
     {
         basis_dr_Y[i] = cokernels_J_basis[i];
         reverse_dict(basis_dr_Y_dict[i], basis_dr_Y[i]);
+        dim_dr_Y += cokernels_J_basis[i].length();
+
 
         basis_dr_X[i] = cokernels_I_basis[i];
         reverse_dict(basis_dr_X_dict[i], basis_dr_X[i]);
-
-
+        dim_dr_X += cokernels_I_basis[i].length();
     }
     if(verbose > 2)
         cout<<"dr::init_solve_and_cokernels() done"<<endl;
@@ -488,7 +504,7 @@ void dr<R>::init_solve_and_cokernels()
 template<typename R>
 void dr<R>::init_cokernels_I_basis()
 {
-    if(verbose > 2)
+    if(verbose > 1)
         cout<<"dr::init_cokernels_I_basis()"<<endl;
     cokernels_I_dimensions.SetLength(n + 1, 0);
     cokernels_I_basis.SetLength(n + 1);
@@ -500,7 +516,7 @@ void dr<R>::init_cokernels_I_basis()
         cokernels_I_dimensions[d] = cokernels_I_basis[d].length();
     }
     dim_I = sum(cokernels_I_dimensions);
-    if(verbose > 0)
+    if(verbose > 1)
         cout << "dim I_i = "<<cokernels_I_dimensions<<endl;
     if(verbose > 2)
         cout<<"dr::init_cokernels_I_basis() done"<<endl;
@@ -511,7 +527,7 @@ void dr<R>::init_cokernels_I_basis()
 template<typename R>
 void dr<R>::init_cokernels_I_basis(int64_t d)
 {
-    if(verbose > 3)
+    if(verbose > 2)
         cout<<"dr::init_cokernels_I_basis("<<d<<")"<<endl;
 
     //I_d = (S^* + J(f) / J(f))_d
@@ -533,7 +549,7 @@ void dr<R>::init_cokernels_I_basis(int64_t d)
     for(i = 0; i < nonpivots.length(); i++)
         cokernels_I_basis[d][i] = tuple_int_list[d][nonpivots[i]];
 
-    if(verbose > 3)
+    if(verbose > 2)
         cout<<"dr::init_cokernels_I_basis("<<d<<") done"<<endl;
 }
 
@@ -543,6 +559,10 @@ void dr<R>::init_cokernels_I_basis(int64_t d)
 template<typename R>
 void dr<R>::matrix_J(Mat<R> &result, int64_t d)
 {
+    if(verbose > 2)
+        cout<<"dr:::matrix_J(-, "<<d<<")"<<endl;
+
+
     //free storage and make 0 x 0 
     result.kill();
     assert( d >= 0 );
@@ -561,6 +581,8 @@ void dr<R>::matrix_J(Mat<R> &result, int64_t d)
             for( int64_t i = 0; i < n + 1; i++)
                 result[tuple_dict[d][fit->first + v]][ i * tuple_list[d - 1].length() + j] = fit->first[i] * fit->second;
     }
+    if(verbose > 2)
+        cout<<"dr:::matrix_J(-, "<<d<<") done"<<endl;
 }
 
 
@@ -597,6 +619,10 @@ inline void dr<ZZ>::cokernel_intersection(Vec<int64_t>  &res, Mat<ZZ> &T, Mat<ZZ
 template<typename R>
 void dr<R>::init_rho_and_pi_matrices()
 {
+    if(verbose > 2)
+        cout<<"dr:init_rho_and_pi_matrices()"<<endl;
+
+
     int64_t i, j, k, d;
     rho.SetLength(n + 2);
     rho_den.SetLength(n + 2);
@@ -635,6 +661,7 @@ void dr<R>::init_rho_and_pi_matrices()
 
         for(i = 0; i < n + 2; i++)
             RHO[i].SetDims( Htuples.length(), Gtuples.length() );
+
         PI.SetDims( cokernels_J_basis[d].length(), Gtuples.length() );
 
         for(j = 0; j < Gtuples.length(); j++)
@@ -684,11 +711,17 @@ void dr<R>::init_rho_and_pi_matrices()
             }
         }
     }
+    if(verbose > 2)
+        cout<<"dr:init_rho_and_pi_matrices() done"<<endl;
+
 }
 
 template<typename R>
 void dr<R>::init_inclusion_matrices()
 {
+    if(verbose > 2)
+        cout<<"dr:init_inclusion_matrices()"<<endl;
+
     // for u in P1 the matrix representing
     // I_u : J_0 + ... + J_n --> P_1 + P_2 + ... + P_{n+1}
     // where J_i --> P_{i+1} corresponds to 
@@ -724,11 +757,15 @@ void dr<R>::init_inclusion_matrices()
         }
 
     }
+    if(verbose > 2)
+        cout<<"dr:init_inclusion_matrices() done"<<endl;
 }
 
 template<typename R>
 void dr<R>::test_inclusion_matrices()
 {
+    if(verbose > 2)
+        cout<<"dr:test_inclusion_matrices()"<<endl;
     int64_t i, j, k;
     Vec< Vec<int64_t> > B, BJ;
     for(i = 1; i < n + 2; i++)
@@ -757,13 +794,16 @@ void dr<R>::test_inclusion_matrices()
         }
 
     }
-
-    
+    if(verbose > 2)
+        cout<<"dr:test_inclusion_matrices() done"<<endl;
 }
 
 template<typename R>
 void dr<R>::init_last_reduction()
 {
+    if(verbose > 2)
+        cout<<"dr:init_last_reduction()"<<endl;
+
     //M: P_1 + .... + P_{n + 1} --- > P_1 + J_2 + ... + J_n
     // (i - 1)! ai \in P^{i} ---> P_1 + J_2 +... + J_i
     Mat<R> &M = last_reduction;
@@ -773,26 +813,30 @@ void dr<R>::init_last_reduction()
 
     Vec<int64_t> shift_rows, shift_columns;
 
-    shift_rows.SetLength(n + 1);
-    shift_columns.SetLength(n + 1);
+    shift_rows.SetLength(n + 2, 0);
+    shift_columns.SetLength(n + 2, 0);
     // shift_rows[k] = dim(P_1 + J_2 + ... + J_{k - 1})
     // shift_columns[k] = dim(P_1 + P_2 + .... + P_{k - 1}
     int64_t total_columns, total_rows;
     total_columns = 0;
     total_rows = 0;
-    for(k = 1; k < n + 1; k++)
+    for(k = 1; k < n + 2; k++)
     {
         shift_columns[k] = total_columns;
         shift_rows[k] = total_rows;
         total_columns += tuple_list[k].length();
-        total_rows += basis_dr_Y[k].length();
+        if(k < n + 1)
+            total_rows += basis_dr_Y[k].length();
     }
     M.SetDims(total_rows, total_columns);
 
     //P_1 --> P_1 is the identity map
     for(k = 1; k < tuple_list[1].length(); k++)
         M[k][k] = 1;
-    
+    D = 1;
+    for(k = 2; k < n + 2; k++)
+        D *= rho_den[k];
+
     int64_t factorial = 1;
     for(k = 2; k < n + 2; k++)
     {
@@ -811,88 +855,104 @@ void dr<R>::init_last_reduction()
             // rho[k][0].column(j).list() \in P_{k-1}
             Vec<R> G, H;
             // G = [0]*dim(P_1 + ... + P_{k-2}) +  rho[k][0].column(j) + [0]*dim(P_k + ... P_{n+1} 
-            G.SetLength(total_rows, R(0));
+            G.SetLength(total_columns, R(0));
             for(i = 0; i < rho[k][0].NumRows(); i++)
                 G[i + shift_columns[k - 1]] = rho[k][0][i][j];
-            
             H = (M * G);
             if( rho_den[k] != 1)
                 for(i = 0; i < H.length(); i++)
                     H[i] /= rho_den[k];
-
             for(i = 0; i < H.length(); i++)
                 M[i][ shift_columns[k] + j] += H[i];
         }
         factorial *= k;
     }
+    if(verbose > 2)
+        cout<<"dr:init_last_reduction() done"<<endl;
 }
 
 template<typename R>
 void dr<R>::test_last_reduction()
 {
-    if( verbose > 0 )
-        cout << "dr.test_last_reduction()" << endl;
+    if( verbose > 2 )
+        cout << "dr::test_last_reduction()" << endl;
 
     Vec< Vec<int64_t> > B;
-    int64_t i, j, k;
+    int64_t i, j, k, shift;
     
     for(i = 0; i < basis_dr_Y.length(); i++)
         B.append( basis_dr_Y[i] );
 
+    init_f_power(n + 1);
     for(i = 0; i < B.length(); i++)
     {
         Vec<int64_t> &v = B[i];
         Vec<R> expected;
-        expected.SetLength(B.length(), 0);
+        expected.SetLength(B.length(), R(0));
         expected[i] = last_reduction_den;
         for(k = 0; k < n + 2 - v[0]; k++)
         {
             Vec<R> G;
-            G.SetLength(last_reduction.NumCols());
-                //FIXME
+            G.SetLength(last_reduction.NumCols(), R(0));
+            shift = 0;
+            for(j = 1; j < k + v[0]; j++)
+                shift += tuple_list[j].length();
+
+            typename map< Vec<int64_t>, R, vi64less>::const_iterator fkit;
+            for(fkit = f_power[k].cbegin(); fkit != f_power[k].cend(); fkit++)
+                G[shift + tuple_dict[k + v[0]][fkit->first + v]] += fkit->second;
+            
+            assert_print(factorial<R>(k + v[0] - 1) * expected, ==, last_reduction * G);
         }
-
-
     }
-
-    /*
-
-        for l, v in enumerate(B):
-            expected = vector([0] * len(B));
-            expected[l] = self.last_reduction[1] ;
-            for k in range(self.n + 2 - v[0]):
-                G = vector([0] * sum( map(len, self.tuple_list[1:self.n + 2])))
-                shift = sum( map(len, self.tuple_list[1:k + v[0]]));
-                for mon, cf in self.get_f_power(k).iteritems():
-                    G[shift + self.tuple_dict[k + v[0]][immutable(mon + v)]] += cf;
-
-                assert self.R(factorial(k + v[0] - 1)) * expected ==  (self.last_reduction[0] * G)
-    */
+    if( verbose > 2 )
+        cout << "dr::test_last_reduction() done" << endl;
 }
 
 
 template<typename R>
 void dr<R>::init_proj()
 {
-    //FIXME
-    /*
-     * ef project_dR(self):
-        if self.verbose > 2:
-            print "dr.project_dR()"
+    if( verbose > 2)
+        cout << "dr::project_dR()" << endl;
 
-        FdR = Matrix(ZZ, self.dim_dR, self.dim_dR_T)
-        FnotdR = Matrix(ZZ, self.dim_dR_T, self.dim_dR_T)
-        B = [];
-        for TL in self.basis_dR_T:
-            B += TL
-        
-        for k, ck in enumerate(B):
-            if ck in self.basis_dR[ck[0]]:
-                FdR[sum(map(len,  self.basis_dR[:ck[0]])) + self.basis_dR_dict[ck[0]][ck], k] = 1
-            else:
-                FnotdR[k, k] = 1
-        return FdR, FnotdR
+    proj_X.SetDims( dim_dr_X, dim_dr_Y);
+    proj_notX.SetDims( dim_dr_Y, dim_dr_Y);
 
-        */
+    int64_t i, j, shiftX, shiftY;
+    shiftX = 0;
+    shiftY = 0;
+    for(i = 1; i < n + 1; i++)
+    {
+        for(j = 0; j < basis_dr_Y[i].length(); j++)
+        {
+            Vec<int64_t> &v = basis_dr_Y[i][j];
+            if( basis_dr_X_dict[i].count(v) == 1)
+                proj_X[shiftX + basis_dr_X_dict[i][v]][ shiftY + j] = 1;
+            else
+                proj_notX[shiftY + j][shiftY + j] = 1;
+        }
+        shiftX += basis_dr_X[i].length();
+        shiftY += basis_dr_Y[i].length();
+
+    }
+    if( verbose > 2)
+        cout << "dr::project_dR() done" << endl;
+
+
 }
+template<typename R>
+void dr<R>::test_all()
+{
+    if( verbose > 0)
+        cout << "dr::test_all()" << endl;
+    test_last_reduction();
+    test_inclusion_matrices();
+    //FIXME
+    //test_monomial_to_basis();
+
+    if( verbose > 0)
+        cout << "dr::test_all() done" << endl;
+}
+
 #endif
