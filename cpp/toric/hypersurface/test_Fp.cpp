55d7f29bd9a49547443c6c3b0255d38cc27245e8
Edgar Costa <edgarcosta@nyu.edu>
2017-06-05 10:46:08 +0200

    verbose bool -> int

--- cpp/toric/hypersurface/test_Fp.cpp
+++ cpp/toric/hypersurface/test_Fp.cpp
@@ -3,7 +3,7 @@
 
 #include "hypersurface.h"
 
-bool test_Fp(const char *input, const char *output, bool verbose)
+bool test_Fp(const char *input, const char *output, int64_t verbose)
 {
     stringstream buffer;
     
