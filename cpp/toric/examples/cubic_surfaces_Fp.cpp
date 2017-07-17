1840cb2d14e893e5dcd4e15cc3e4249c442f7bfc
Edgar Costa <edgarcosta@nyu.edu>
2017-07-17 10:11:38 -0400

    examples refactured

--- cpp/toric/examples/cubic_surfaces_Fp.cpp
+++ cpp/toric/examples/cubic_surfaces_Fp.cpp
@@ -3,71 +3,34 @@
 
 #include "hypersurface.h"
 
+#define verbose 0
+
 int main()
 {
-    bool val = true;
-    timestamp_type wtime1, wtime2;
-    double wall_time, user_time;
-    const char cubic[] = "47 \n[ [0 3 0][0 1 2][1 1 0][0 0 0][0 0 1][2 0 0][1 0 2][1 1 1][1 2 0][3 0 0][1 0 0][0 2 1][2 0 1][0 0 3][1 0 1][0 1 1][0 0 2][2 1 0][0 1 0][0 2 0] ]\n[42 26 5 45 26 26 26 5 26 41 26 26 26 43 5 5 26 26 26 26] \n[ [-1 -1 -1][ 0  0  1][ 0  1  0][ 1  0  0] ] \n[3 0 0 0] \n";
-    const char cubic_zeta[] = "[ -10779215329 0 0 0 0 0 1]";
-    user_time = get_cpu_time();
-    get_timestamp(&wtime1);
-    cout << "Testing: cubic surface p = 47" <<endl;
-    if( test_Fp(cubic, cubic_zeta, 0))
-        cout <<"PASS"<<endl;
-    else
-        val = false;
-    get_timestamp(&wtime2);
-    wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-    user_time = get_cpu_time() - user_time;
-    printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-
-
-    const char cubic2[] = "1009 \n[ [0 3 0][0 1 2][1 1 0][0 0 0][0 0 1][2 0 0][1 0 2][1 1 1][1 2 0][3 0 0][1 0 0][0 2 1][2 0 1][0 0 3][1 0 1][0 1 1][0 0 2][2 1 0][0 1 0][0 2 0] ]\n[1004 988 967 1007 988 988 988 967 988 1003 988 988 988 1005 967 967 988 988 988 988] \n[ [-1 -1 -1][ 0  0  1][ 0  1  0][ 1  0  0] ] \n[3 0 0 0] \n";
-    const char cubic2_zeta[] = "[1055229678769825441 1045817322864049 0 -1027243729 0 1009 1]";
-    user_time = get_cpu_time();
-    get_timestamp(&wtime1);
-    cout << "Testing: cubic surface p = 1009" <<endl;
-    if( test_Fp(cubic2, cubic2_zeta, 0))
-        cout <<"PASS"<<endl;
-    else
-        val = false;
-    get_timestamp(&wtime2);
-    wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-    user_time = get_cpu_time() - user_time;
-    printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-    
-    const char cubic3[] = "65537 \n[ [0 3 0][0 1 2][1 1 0][0 0 0][0 0 1][2 0 0][1 0 2][1 1 1][1 2 0][3 0 0][1 0 0][0 2 1][2 0 1][0 0 3][1 0 1][0 1 1][0 0 2][2 1 0][0 1 0][0 2 0] ]\n[65532 65516 65495 65535 65516 65516 65516 65495 65516 65531 65516 65516 65516 65533 65495 65495 65516 65516 65516 65516] \n[ [-1 -1 -1][ 0  0  1][ 0  1  0][ 1  0  0] ] \n[3 0 0 0] \n";
-    const char cubic3_zeta[] = "[79235416345888816038194577409 0 -18447869999386460161 0 -4295098369 0 1]";
-    user_time = get_cpu_time();
-    get_timestamp(&wtime1);
-    cout << "Testing: cubic surface p = 2^16 + 1" <<endl;
-    if( test_Fp(cubic3, cubic3_zeta, 0))
-        cout <<"PASS"<<endl;
-    else
-        val = false;
-    get_timestamp(&wtime2);
-    wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-    user_time = get_cpu_time() - user_time;
-    printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-
-/*
-    const char cubic4[] = "1048583 \n[ [0 3 0][0 1 2][1 1 0][0 0 0][0 0 1][2 0 0][1 0 2][1 1 1][1 2 0][3 0 0][1 0 0][0 2 1][2 0 1][0 0 3][1 0 1][0 1 1][0 0 2][2 1 0][0 1 0][0 2 0] ]\n[1048578 1048562 1048541 1048581 1048562 1048562 1048562 1048541 1048562 1048577 1048562 1048562 1048562 1048579 1048541 1048541 1048562 1048562 1048562 1048562] \n[ [-1 -1 -1][ 0  0  1][ 0  1  0][ 1  0  0] ] \n[3 0 0 0] \n";
-    const char cubic4_zeta[] = "[1329281237998693845036542985209236369 -1267692913196851222112644383143 0 0 0 -1048583 1]";
-    user_time = get_cpu_time();
-    get_timestamp(&wtime1);
-    cout << "Testing: cubic surface p = 2^20 + 7" <<endl;
-    if( test_Fp(cubic4, cubic4_zeta, 0))
-        cout <<"PASS"<<endl;
-    else
-        val = false;
-    get_timestamp(&wtime2);
-    wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-    user_time = get_cpu_time() - user_time;
-    printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-*/
-
-
-
-    return not val;
+    int examples_length = 4;
+    char examples[][3][buffer_length] =
+    {
+        {
+            " cubic surface p = 47",
+            "47 \n[ [0 3 0][0 1 2][1 1 0][0 0 0][0 0 1][2 0 0][1 0 2][1 1 1][1 2 0][3 0 0][1 0 0][0 2 1][2 0 1][0 0 3][1 0 1][0 1 1][0 0 2][2 1 0][0 1 0][0 2 0] ]\n[42 26 5 45 26 26 26 5 26 41 26 26 26 43 5 5 26 26 26 26] \n[ [-1 -1 -1][ 0  0  1][ 0  1  0][ 1  0  0] ] \n[3 0 0 0] \n",
+            "[ -10779215329 0 0 0 0 0 1]"
+        },
+        {
+            "cubic surface p = 1009",
+            "1009 \n[ [0 3 0][0 1 2][1 1 0][0 0 0][0 0 1][2 0 0][1 0 2][1 1 1][1 2 0][3 0 0][1 0 0][0 2 1][2 0 1][0 0 3][1 0 1][0 1 1][0 0 2][2 1 0][0 1 0][0 2 0] ]\n[1004 988 967 1007 988 988 988 967 988 1003 988 988 988 1005 967 967 988 988 988 988] \n[ [-1 -1 -1][ 0  0  1][ 0  1  0][ 1  0  0] ] \n[3 0 0 0] \n",
+            "[1055229678769825441 1045817322864049 0 -1027243729 0 1009 1]"
+        },
+        {
+            "cubic surface p = 65537",
+            "65537 \n[ [0 3 0][0 1 2][1 1 0][0 0 0][0 0 1][2 0 0][1 0 2][1 1 1][1 2 0][3 0 0][1 0 0][0 2 1][2 0 1][0 0 3][1 0 1][0 1 1][0 0 2][2 1 0][0 1 0][0 2 0] ]\n[65532 65516 65495 65535 65516 65516 65516 65495 65516 65531 65516 65516 65516 65533 65495 65495 65516 65516 65516 65516] \n[ [-1 -1 -1][ 0  0  1][ 0  1  0][ 1  0  0] ] \n[3 0 0 0] \n",
+            "[79235416345888816038194577409 0 -18447869999386460161 0 -4295098369 0 1]"
+        },
+        {
+            "cubic surface p = 1048583, long time ~ 500s",
+            "1048583 \n[ [0 3 0][0 1 2][1 1 0][0 0 0][0 0 1][2 0 0][1 0 2][1 1 1][1 2 0][3 0 0][1 0 0][0 2 1][2 0 1][0 0 3][1 0 1][0 1 1][0 0 2][2 1 0][0 1 0][0 2 0] ]\n[1048578 1048562 1048541 1048581 1048562 1048562 1048562 1048541 1048562 1048577 1048562 1048562 1048562 1048579 1048541 1048541 1048562 1048562 1048562 1048562] \n[ [-1 -1 -1][ 0  0  1][ 0  1  0][ 1  0  0] ] \n[3 0 0 0] \n",
+            "[1329281237998693845036542985209236369 -1267692913196851222112644383143 0 0 0 -1048583 1]"
+        }
+    };
+    return not run_examples(examples, examples_length, verbose);
 }
+
--- cpp/toric/examples/curves_Fp.cpp
+++ cpp/toric/examples/curves_Fp.cpp
@@ -2,117 +2,62 @@
 // See LICENSE file for license details.
 
 #include "hypersurface.h"
+#define verbose 0
 
 int main()
 {
-    bool val = true;
-    timestamp_type wtime1, wtime2;
-    double wall_time, user_time;
+    int examples_length = 8;
+    char examples[10][3][buffer_length] = {
+        // f = -x^3 + x^2 + y^2 + 7820*x + y + 263580
+        {
+            "elliptic curve 11.a p = 17",
+            "17 \n[ [0 1][0 0][3 0][1 1][2 0][0 2] ]\n[1 12 16 0 1 1] \n[ [ 0  1][ 1  0][-2 -3] ] \n[0 0 6] \n",
+            "[17 2 1]"
+        },
+        // f = 2*x^5 + 4*x^3 + x^2 - y^2 + 2*x + 1
+        {
+            "genus 2 curve 688.a  p = 31",
+            "31 \n[ [0 0][3 0][2 1][0 2][2 0][5 0][1 0][1 1] ]\n[1 4 0 30 1 2 2 0] \n[ [ 0  1][ 1  0][-2 -5] ] \n[0 0 10] \n",
+            "[961 -31 30 -1 1]"
+        },
+        // f = x^6 + y^2 - 31*y + 240
+        {
+            "genus 2 curve 11664.a  p = 31",
+            "31 \n[ [0 0][0 2][1 1][2 1][6 0] ]\n[23 1 0 0 1] \n[ [ 1  0][ 0  1][-1 -3] ] \n[0 0 6] \n",
+            "[961 0 46 0 1]"
+        },
+        // f = x^4 - y^3 + x + 1
+        {
+            "C_{3,4} curve p = 43",
+            "43 \n[ [1 2][0 0][2 1][1 1][1 0][0 3][4 0] ]\n[0 1 0 0 1 42 1] \n[ [ 0  1][ 1  0][-3 -4] ] \n[0 0 12] \n",
+            "[79507 27735 6579 1258 153 15 1]"
+        },
+        // f = -2*x^4 - 3*x^3*y - x^2*y^2 + x*y^3 + y^4 - x*y^2 + 7*y^3 + 5*x^2 - 2*x*y - 2*y^2 + 6*y + 1
+        {
+            "plane curve, genus 3 p = 17",
+            "17 \n[ [1 2][0 1][1 3][3 1][2 1][1 1][2 0][0 0][2 2][0 4][0 3][0 2][4 0] ]\n[16 6 1 14 0 15 5 1 16 1 7 15 15] \n[ [-1 -1][ 0  1][ 1  0] ] \n[4 0 0] \n",
+            "[4913 867 221 27 13 3 1]"
+        },
+        // f = y^4 - x^3 + x^-1*y^-1
+        {
+            "genus 9 curve p = 37",
+            "37 \n[ [ 0  1][ 1  2][ 0  0][ 3  0][ 0  4][ 2  1][ 0  2][ 2  0][-1 -1][ 1  0][ 0  3][ 1  1] ]\n[0 0 0 36 1 0 0 0 1 0 0 0] \n[ [ 5 -1][-1  4][-4 -3] ] \n[4 3 12] \n",
+            "[129961739795077 0 31612315085289 0 3417547576788 0 215521018356 0 8737338582 0 236144286 0 4254852 0 49284 0 333 0 1]"
+        },
+        // f = x^22 - y^2 + x + 1
+        {
+            "hyperelliptic curve of genus 10 p = 13",
+            "13 \n[ [ 0  0][ 9  1][ 7  1][ 8  1][ 6  1][ 3  1][ 1  1][ 2  1][22  0][10  1][ 5  1][ 1  0][ 4  1][ 0  2] ]\n[1 0 0 0 0 0 0 0 1 0 0 1 0 12] \n[ [  0   1][  1   0][ -1 -11] ] \n[0 0 22] \n",
+            "[137858491849 31813498119 8973037931 3074677333 323396203 24876631 10995985 12881011 3433911 999635 664001 76895 20319 5863 385 67 67 49 11 3 1]"
+        },
+        // f = x^22 - y^2 + x + 1
+        {
+            "hyperelliptic curve of genus 10 p = 103",
+            "103 \n[ [ 0  0][ 9  1][ 7  1][ 8  1][ 6  1][ 3  1][ 1  1][ 2  1][22  0][10  1][ 5  1][ 1  0][ 4  1][ 0  2] ]\n[1 0 0 0 0 0 0 0 1 0 0 1 0 102] \n[ [  0   1][  1   0][ -1 -11] ] \n[0 0 22] \n",
+            "[134391637934412192049 5219092735316978332 -152012409766513932 -105769152426538820 186272158258524 -534123936992982 124637044805304 8798421444054 969104502946 -108512162008 -4925241640 -1053516136 91347394 8051802 1107384 -46074 156 -860 -12 4 1]"
+        },
 
-// elliptic curve 11.a p = 17
-const char ecp[] = "17 \n[ [0 2][0 0][3 0][2 0][0 1] ]\n[1 12 16 1 1] \n[ [ 0  1][ 1  0][-2 -3] ] \n[0 0 6] \n";
-const char ecp_zeta[] = "[17 2 1]";
-cout << "Testing: elliptic curve 11.a p = 17" <<endl;
-user_time = get_cpu_time();
-get_timestamp(&wtime1);
-if( test_Fp(ecp, ecp_zeta, 0))
-cout <<"PASS"<<endl;
-else
-val = false;
-get_timestamp(&wtime2);
-wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-user_time = get_cpu_time() - user_time;
-printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-
-// genus 2 curve 11664.a  p = 31
-const char g2p[] = "31 \n[ [6 0][0 0][0 2] ]\n[1 23 1] \n[ [ 1  0][ 0  1][-1 -3] ] \n[0 0 6] \n";
-const char g2p_zeta[] = "[961 0 46 0 1]";
-cout << "Testing: genus 2 curve 11664.a  p = 31" <<endl;
-user_time = get_cpu_time();
-get_timestamp(&wtime1);
-if( test_Fp(g2p, g2p_zeta, 0))
-cout <<"PASS"<<endl;
-else
-val = false;
-get_timestamp(&wtime2);
-wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-user_time = get_cpu_time() - user_time;
-printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-
-// C_{3,4} curve p = 43
-const char c34[] = "43 \n[ [0 3][0 0][4 0][1 0] ]\n[42 1 1 1] \n[ [ 0  1][ 1  0][-3 -4] ] \n[0 0 12] \n";
-const char c34_zeta[] = "[79507 27735 6579 1258 153 15 1]";
-cout << "Testing: C_{3,4} curve p = 43" <<endl;
-user_time = get_cpu_time();
-get_timestamp(&wtime1);
-if( test_Fp(c34, c34_zeta, 0))
-cout <<"PASS"<<endl;
-else
-val = false;
-get_timestamp(&wtime2);
-wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-user_time = get_cpu_time() - user_time;
-printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-
-// plane curve, genus 3 p = 17
-const char pg3[] = "17 \n[ [0 3][0 0][3 1][2 2][1 3][1 2][0 1][4 0][1 1][0 2][2 0][0 4] ]\n[7 1 14 16 1 16 6 15 15 15 5 1] \n[ [-1 -1][ 0  1][ 1  0] ] \n[4 0 0] \n";
-const char pg3_zeta[] = "[4913 867 221 27 13 3 1]";
-cout << "Testing: plane curve, genus 3 p = 17" <<endl;
-user_time = get_cpu_time();
-get_timestamp(&wtime1);
-if( test_Fp(pg3, pg3_zeta, 0))
-cout <<"PASS"<<endl;
-else
-val = false;
-get_timestamp(&wtime2);
-wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-user_time = get_cpu_time() - user_time;
-printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-
-// genus 9 curve p = 37
-const char g9[] = "37 \n[ [-1 -1][ 0  4][ 3  0] ]\n[1 1 36] \n[ [ 5 -1][-1  4][-4 -3] ] \n[4 3 12] \n";
-const char g9_zeta[] = "[129961739795077 0 31612315085289 0 3417547576788 0 215521018356 0 8737338582 0 236144286 0 4254852 0 49284 0 333 0 1]";
-cout << "Testing: genus 9 curve p = 37" <<endl;
-user_time = get_cpu_time();
-get_timestamp(&wtime1);
-if( test_Fp(g9, g9_zeta, 0))
-cout <<"PASS"<<endl;
-else
-val = false;
-get_timestamp(&wtime2);
-wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-user_time = get_cpu_time() - user_time;
-printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-
-// hyperelliptic curve of genus 10 p = 13
-const char g10[] = "13 \n[ [22  0][ 1  0][ 0  0][ 0  2] ]\n[1 1 1 12] \n[ [  0   1][  1   0][ -1 -11] ] \n[0 0 22] \n";
-const char g10_zeta[] = "[137858491849 31813498119 8973037931 3074677333 323396203 24876631 10995985 12881011 3433911 999635 664001 76895 20319 5863 385 67 67 49 11 3 1]";
-cout << "Testing: hyperelliptic curve of genus 10 p = 13" <<endl;
-user_time = get_cpu_time();
-get_timestamp(&wtime1);
-if( test_Fp(g10, g10_zeta, 0))
-cout <<"PASS"<<endl;
-else
-val = false;
-get_timestamp(&wtime2);
-wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-user_time = get_cpu_time() - user_time;
-printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-
-// hyperelliptic curve of genus 10 p = 103
-const char g10_103[] = "103 \n[ [22  0][ 1  0][ 0  0][ 0  2] ]\n[1 1 1 102] \n[ [  0   1][  1   0][ -1 -11] ] \n[0 0 22] \n";
-const char g10_103_zeta[] = "[134391637934412192049 5219092735316978332 -152012409766513932 -105769152426538820 186272158258524 -534123936992982 124637044805304 8798421444054 969104502946 -108512162008 -4925241640 -1053516136 91347394 8051802 1107384 -46074 156 -860 -12 4 1]";
-cout << "Testing: hyperelliptic curve of genus 10 p = 103" <<endl;
-user_time = get_cpu_time();
-get_timestamp(&wtime1);
-if( test_Fp(g10_103, g10_103_zeta, 0))
-cout <<"PASS"<<endl;
-else
-val = false;
-get_timestamp(&wtime2);
-wall_time = timestamp_diff_in_seconds(wtime1,wtime2);
-user_time = get_cpu_time() - user_time;
-printf("Time: CPU %.2f s, Wall: %.2f s\n\n\n", user_time, wall_time );
-
-return not val;
+    };
+    return not run_examples(examples, examples_length, verbose);
 }
+
